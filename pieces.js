4dcbc617190d1166c0df1040dc4e77aff88e719d
Antonin <antochevolleau@gmail.com>
2025-06-06 14:42:44 +0200

    feat: Affichage de toutes les pièces autos + Modification du CSS

--- pieces.js
+++ pieces.js
@@ -2,39 +2,41 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-// Récupération de la première pièce
-const article = pieces[0];
+// Boucle de récupération et d'affichage des données des pièces
+for (let i = 0; i < pieces.length; i++) {
+  const sectionFiches = document.querySelector(".fiches");
+  const pieceElement = document.createElement("article");
 
-// Récupération des données de la première pièce
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+  // Récupération des données de la pièce
+  const imageElement = document.createElement("img");
+  imageElement.src = pieces[i].image;
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+  const nomElement = document.createElement("h2");
+  nomElement.innerText = pieces[i].nom;
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix : ${article.prix} € (${
-  article.prix < 35 ? "€" : "€€€"
-})`;
+  const prixElement = document.createElement("p");
+  prixElement.innerText = `Prix : ${pieces[i].prix} € (${
+    pieces[i].prix < 35 ? "€" : "€€€"
+  })`;
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+  const categorieElement = document.createElement("p");
+  categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText =
-  article.description ?? "Pas de description pour le moment";
+  const descriptionElement = document.createElement("p");
+  descriptionElement.innerText =
+    pieces[i].description ?? "Pas de description pour le moment";
 
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite
-  ? "En stock"
-  : "Rupture de stock";
+  const disponibiliteElement = document.createElement("p");
+  disponibiliteElement.innerText = pieces[i].disponibilite
+    ? "En stock"
+    : "Rupture de stock";
 
-// Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+  // Rattachement de nos balises au DOM
+  sectionFiches.appendChild(pieceElement);
+  pieceElement.appendChild(imageElement);
+  pieceElement.appendChild(nomElement);
+  pieceElement.appendChild(prixElement);
+  pieceElement.appendChild(categorieElement);
+  pieceElement.appendChild(descriptionElement);
+  pieceElement.appendChild(disponibiliteElement);
+}
